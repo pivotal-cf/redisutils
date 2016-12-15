2d930955dc505ef3a83a81e3a62dbc98ea6c73a9
BooleanCat <thgodkin@gmail.com>
2016-12-15 00:42:59 +0000

    wait for monit to report foo running in integration tests

--- docker/test.sh
+++ docker/test.sh
@@ -2,9 +2,23 @@
 
 REDISUTILS_DIR=$GOPATH/src/github.com/pivotal-cf/redisutils
 
+while [[ $# -gt 0 ]]; do
+  case $1 in
+      --verbose)
+        VERBOSE="-v"
+      ;;
+
+      *)
+        echo "Usage: test.sh [--verbose]"
+        exit 1
+      ;;
+  esac
+  shift
+done
+
 cp -r $HOME/redisutils/vendor/* $GOPATH/src
 mkdir -p $REDISUTILS_DIR
 cp -r $HOME/redisutils/* $REDISUTILS_DIR
 
 cd $REDISUTILS_DIR
-ginkgo -r --race
+ginkgo $VERBOSE -r --race
--- monit/integration/monit_integration_suite_test.go
+++ monit/integration/monit_integration_suite_test.go
@@ -1,6 +1,10 @@
 package monit_integration_test
 
 import (
+	"os/exec"
+	"regexp"
+	"time"
+
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 
@@ -10,4 +14,42 @@ import (
 func TestMonitIntegration(t *testing.T) {
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "Monit Integration Suite")
+}
+
+var _ = BeforeSuite(func() {
+	startMonit()
+	waitForFooRunning()
+})
+
+func waitForFooRunning() {
+	interval := time.Millisecond * 200
+	timeout := time.Second * 15
+
+	for elapsed := time.Duration(0); elapsed < timeout; elapsed = elapsed + interval {
+		if fooIsRunning() {
+			return
+		}
+		time.Sleep(interval)
+	}
+
+	Fail("timed out waiting for monit to start")
+}
+
+func fooIsRunning() bool {
+	summary := getMonitSummary()
+	pattern := regexp.MustCompile(`(?m)^Process 'foo'\s+running$`)
+	return pattern.MatchString(summary)
+}
+
+func startMonit() {
+	cmd := exec.Command("monit", "-c", "/home/vcap/monitrc")
+	err := cmd.Run()
+	Expect(err).NotTo(HaveOccurred())
+}
+
+func getMonitSummary() string {
+	cmd := exec.Command("monit", "-c", "/home/vcap/monitrc", "summary")
+	summary, err := cmd.CombinedOutput()
+	Expect(err).NotTo(HaveOccurred())
+	return string(summary)
 }

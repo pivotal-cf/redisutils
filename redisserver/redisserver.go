a4e64fffb80a5544ba02e5aad1bc53ea7b09fc65
BooleanCat <thgodkin@gmail.com>
2016-12-30 16:26:15 +0000

    stubs for start/stop methods

--- redisserver/redisserver.go
+++ redisserver/redisserver.go
@@ -1,1 +1,25 @@
 package redisserver
+
+//RedisServer is a controller for `redis-server`
+type RedisServer interface {
+	Start() error
+	Stop() error
+}
+
+//Server is a controller for `redis-server`
+type Server struct{}
+
+//New is the correct way to initialise a Server
+func New() *Server {
+	return new(Server)
+}
+
+//Start launches a `redis-server`
+func (server *Server) Start() error {
+	return nil
+}
+
+//Stop kills a running `redis-server`
+func (server *Server) Stop() error {
+	return nil
+}
--- redisserver/redisserver_test.go
+++ redisserver/redisserver_test.go
@@ -1,5 +1,38 @@
 package redisserver
 
-import . "github.com/onsi/ginkgo"
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
 
-var _ = Describe("redisserver", func() {})
+var _ = Describe("redisserver", func() {
+	var server *Server
+
+	BeforeEach(func() {
+		server = New()
+	})
+
+	Describe("#Start", func() {
+		var startErr error
+
+		BeforeEach(func() {
+			startErr = server.Start()
+		})
+
+		It("does not return an error", func() {
+			Expect(startErr).NotTo(HaveOccurred())
+		})
+	})
+
+	Describe("#Stop", func() {
+		var stopErr error
+
+		BeforeEach(func() {
+			stopErr = server.Stop()
+		})
+
+		It("does not return an error", func() {
+			Expect(stopErr).NotTo(HaveOccurred())
+		})
+	})
+})

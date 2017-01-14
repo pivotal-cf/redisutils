349e0674fcd19db4e7863cae6908766dbf1d94c8
BooleanCat <thgodkin@gmail.com>
2017-01-14 16:34:20 +0000

    don't whitebox interface tests

--- iredis/iredis_suite_test.go
+++ iredis/iredis_suite_test.go
@@ -1,4 +1,4 @@
-package iredis
+package iredis_test
 
 import (
 	"testing"
--- iredis/iredis_test.go
+++ iredis/iredis_test.go
@@ -1,53 +1,57 @@
-package iredis
+package iredis_test
 
-import . "github.com/onsi/ginkgo"
+import (
+	. "github.com/onsi/ginkgo"
+
+	"github.com/pivotal-cf/redisutils/iredis"
+)
 
 var _ = Describe("interfaces", func() {
 	Describe("Redis", func() {
 		It("is implemented by iredis.RedisWrap", func() {
-			redisWrap := new(RedisWrap)
-			var _ = Redis(redisWrap)
+			redisWrap := new(iredis.RedisWrap)
+			var _ = iredis.Redis(redisWrap)
 		})
 
 		It("is implemented by iredis.RedisFake", func() {
-			redisFake := new(RedisFake)
-			var _ = Redis(redisFake)
+			redisFake := new(iredis.RedisFake)
+			var _ = iredis.Redis(redisFake)
 		})
 	})
 
 	Describe("Client", func() {
 		It("is implemented by iredis.ClientWrap", func() {
-			clientWrap := new(ClientWrap)
-			var _ = Client(clientWrap)
+			clientWrap := new(iredis.ClientWrap)
+			var _ = iredis.Client(clientWrap)
 		})
 
 		It("is implemented by iredis.ClientFake", func() {
-			clientFake := new(ClientFake)
-			var _ = Client(clientFake)
+			clientFake := new(iredis.ClientFake)
+			var _ = iredis.Client(clientFake)
 		})
 	})
 
 	Describe("StatusCmd", func() {
 		It("is implemented by iredis.StatusCmdWrap", func() {
-			statusCmdWrap := new(StatusCmdWrap)
-			var _ = StatusCmd(statusCmdWrap)
+			statusCmdWrap := new(iredis.StatusCmdWrap)
+			var _ = iredis.StatusCmd(statusCmdWrap)
 		})
 
 		It("is implemented by iredis.StatusCmdFake", func() {
-			statusCmdFake := new(StatusCmdFake)
-			var _ = StatusCmd(statusCmdFake)
+			statusCmdFake := new(iredis.StatusCmdFake)
+			var _ = iredis.StatusCmd(statusCmdFake)
 		})
 	})
 
 	Describe("StringCmd", func() {
 		It("is implemented by iredis.StringCmdWrap", func() {
-			stringCmdWrap := new(StringCmdWrap)
-			var _ = StringCmd(stringCmdWrap)
+			stringCmdWrap := new(iredis.StringCmdWrap)
+			var _ = iredis.StringCmd(stringCmdWrap)
 		})
 
 		It("is implemented by iredis.StringCmdFake", func() {
-			stringCmdFake := new(StringCmdFake)
-			var _ = StringCmd(stringCmdFake)
+			stringCmdFake := new(iredis.StringCmdFake)
+			var _ = iredis.StringCmd(stringCmdFake)
 		})
 	})
 })

f42822fddc719f5e35a338556530d1e2dd0ef85e
Tom Godkin <tgodkin@pivotal.io>
2017-01-16 17:48:03 +0000

    Refactor to improve readability of specs
    [flex]
    
    Signed-off-by: Maria Ntalla <mntalla@pivotal.io>

--- redisconf/redisconf_test.go
+++ redisconf/redisconf_test.go
@@ -6,57 +6,65 @@ import (
 )
 
 var _ = Describe("redisconf", func() {
+	var c Conf
+
 	Describe("Conf", func() {
+		BeforeEach(func() {
+			c = NewConf("hello", "brother", "mine")
+		})
+
 		It("assigns the keyword correctly", func() {
-			var c Conf = NewConf("hello", "brother", "mine")
 			Expect(c.Keyword).To(Equal("hello"))
 		})
 
 		It("assigns the args correctly", func() {
-			var c Conf = NewConf("hello", "brother", "mine")
 			expectedArgs := []string{"brother", "mine"}
 			Expect(c.Args).To(Equal(expectedArgs))
 		})
 
 		Context("without args", func() {
+			BeforeEach(func() {
+				c = NewConf("hello")
+			})
+
 			It("assigns the keyword correctly", func() {
-				c := NewConf("hello")
 				Expect(c.Keyword).To(Equal("hello"))
 			})
 
 			It("assigns the args correctly", func() {
-				c := NewConf("hello")
 				Expect(c.Args).To(BeEmpty())
 			})
 		})
 
 		Describe("String", func() {
 			It("converts to space separated string", func() {
-				c := NewConf("hello", "brother", "mine")
 				Expect(c.String()).To(Equal("hello brother mine"))
 			})
 		})
 	})
 
 	Describe("RedisConf", func() {
-		It("exists", func() {
-			var _ RedisConf = RedisConf{}
-		})
-
 		Describe("Encode", func() {
-			It("encodes a RedisConf as newline separated Confs", func() {
-				rc := RedisConf{
+			var redisConf RedisConf
+
+			BeforeEach(func() {
+				redisConf = RedisConf{
 					NewConf("hello", "brother", "mine"),
 					NewConf("did", "you", "miss", "me"),
 				}
-				Expect(rc.Encode()).To(Equal("hello brother mine\ndid you miss me\n"))
+			})
+
+			It("encodes a RedisConf as newline separated Confs", func() {
+				Expect(redisConf.Encode()).To(Equal("hello brother mine\ndid you miss me\n"))
 			})
 
 			Context("when the RedisConf is empty", func() {
-				rc := RedisConf{}
+				BeforeEach(func() {
+					redisConf = RedisConf{}
+				})
 
 				It("returns an empty string", func() {
-					Expect(rc.Encode()).To(Equal(""))
+					Expect(redisConf.Encode()).To(Equal(""))
 				})
 			})
 		})

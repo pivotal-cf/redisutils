f7c25a3babae0ffb079be2daaaf18eaaaa5cf140
Jonathan Qiwen Tsang <jtsang@pivotal.io>
2017-01-20 14:28:07 +0000

    Finish stringcmd interface
    [#135608673]
    
    Signed-off-by: Tom Godkin <tgodkin@pivotal.io>

--- iredis/stringcmd.go
+++ iredis/stringcmd.go
@@ -5,6 +5,9 @@ import redis "gopkg.in/redis.v5"
 //StringCmd is an interface around redis.StringCmd
 type StringCmd interface {
 	Result() (string, error)
+	Err() error
+	String() string
+	Val() string
 }
 
 //StringCmdReal is a wrapper around redis that implements iredis.StringCmd
@@ -15,4 +18,19 @@ type StringCmdReal struct {
 //Result is a wrapper around redis.StringCmd.Result()
 func (cmd *StringCmdReal) Result() (string, error) {
 	return cmd.stringCmd.Result()
+}
+
+//Err is a wrapper around redis.StringCmd.Err()
+func (cmd *StringCmdReal) Err() error {
+	return cmd.stringCmd.Err()
+}
+
+//String is a wrapper around redis.StringCmd.String()
+func (cmd *StringCmdReal) String() string {
+	return cmd.stringCmd.String()
+}
+
+//Val is a wrapper around redis.StringCmd.Val()
+func (cmd *StringCmdReal) Val() string {
+	return cmd.stringCmd.Val()
 }

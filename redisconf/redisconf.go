27d8a0e04e92be17817337bc174f1ea086b5dcc5
BooleanCat <thgodkin@gmail.com>
2017-01-23 21:11:03 +0000

    implement string for directives

--- redisconf/redisconf.go
+++ redisconf/redisconf.go
@@ -1,1 +1,59 @@
 package redisconf
+
+import "fmt"
+
+//Directive describes the methods for `redis.conf` directives
+type Directive interface {
+	String() string
+	IsValid() error
+}
+
+//RedisConf is a representation of `redis.conf`
+type RedisConf []Directive
+
+//Config is a config value for a `redis.conf`
+type Config struct {
+	Name  string
+	Value string
+}
+
+//NewConfig is a conventient way to initialise a Config
+func NewConfig(name, value string) Config {
+	return Config{Name: name, Value: value}
+}
+
+func (config Config) String() string {
+	return fmt.Sprintf("%s %s", config.Name, config.Value)
+}
+
+//IsValid returns an error if it considered invalid by Redis
+func (config Config) IsValid() error {
+	return nil
+}
+
+//RenameCommand is a command alias for a Redis command
+type RenameCommand struct {
+	Command string
+	Alias   string
+}
+
+//NewRenameCommand is a conventient way to initialise a RenameCommand
+func NewRenameCommand(command, alias string) RenameCommand {
+	return RenameCommand{Command: command, Alias: alias}
+}
+
+func (rename RenameCommand) String() string {
+	return fmt.Sprintf("rename-command %s %s", rename.Command, rename.aliasString())
+}
+
+//IsValid returns an error if it considered invalid by Redis
+func (rename RenameCommand) IsValid() error {
+	return nil
+}
+
+func (rename RenameCommand) aliasString() string {
+	if rename.Alias == "" {
+		return `""`
+	}
+	return rename.Alias
+}
--- redisconf/redisconf_test.go
+++ redisconf/redisconf_test.go
@@ -1,1 +1,43 @@
 package redisconf_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"github.com/pivotal-cf/redisutils/redisconf"
+)
+
+var _ = Describe("redisconf", func() {
+	Describe("Config", func() {
+		It("implements the Directive interface", func() {
+			var _ redisconf.Directive = redisconf.Config{}
+		})
+
+		Describe("#String", func() {
+			It("space separates Name and Value", func() {
+				directive := redisconf.NewConfig("save", "900 1")
+				Expect(directive.String()).To(Equal("save 900 1"))
+			})
+		})
+	})
+
+	Describe("RenameCommand", func() {
+		It("implements the Directive interface", func() {
+			var _ redisconf.Directive = redisconf.RenameCommand{}
+		})
+
+		Describe("#String", func() {
+			It("spaces separates Command and Alias after the rename directive", func() {
+				directive := redisconf.NewRenameCommand("BGSAVE", "foo")
+				Expect(directive.String()).To(Equal("rename-command BGSAVE foo"))
+			})
+
+			Context("when Alias is blank", func() {
+				It("returns empty quotes for its alias part", func() {
+					directive := redisconf.NewRenameCommand("BGSAVE", "")
+					Expect(directive.String()).To(Equal(`rename-command BGSAVE ""`))
+				})
+			})
+		})
+	})
+})

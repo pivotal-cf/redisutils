000ac3883467e1bb3db22278ec6a5a556d2b8c21
BooleanCat <thgodkin@gmail.com>
2017-02-07 15:31:27 +0000

    remove debug prints

--- tunnel/scripts/containers.sh
+++ tunnel/scripts/containers.sh
@@ -1,22 +1,13 @@
 #!/bin/bash -e
 
-GREEN='\033[0;32m'
-NC='\033[0m' # No Colour
-
-function echo_green {
-  echo -e "${GREEN}$1${NC}"
-}
-
 function network_bastion {
   case $1 in
     create)
       docker network create --driver bridge bastion > /dev/null
-      echo_green "Created network bastion"
     ;;
 
     destroy)
       docker network rm bastion > /dev/null
-      echo_green "Destroyed network bastion"
     ;;
   esac
 }
@@ -29,13 +20,11 @@ function redis_server {
         --name redis-server \
         -d -i cflondonservices/redis-server \
          > /dev/null
-      echo_green "Started redis-server container"
     ;;
 
     destroy)
       docker stop ssh-server > /dev/null
       docker rm ssh-server > /dev/null
-      echo_green "Destroyed redis-server container"
     ;;
   esac
 }
@@ -48,13 +37,11 @@ function ssh_server {
         --name ssh-server \
         -d -P cflondonservices/ssh-server \
          > /dev/null
-      echo_green "Started ssh-server container"
     ;;
 
     destroy)
       docker stop redis-server > /dev/null
       docker rm redis-server > /dev/null
-      echo_green "Destroyed ssh-server container"
     ;;
   esac
 }

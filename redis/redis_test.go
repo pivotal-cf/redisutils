100293e89c27e5058bbdf9513911b637dfb0b8de
James Norman <jnorman@pivotal.io>
2019-02-28 10:27:05 +0000

    Switch to official redigo repo
    [#164134755]
    
    Signed-off-by: Ed Cook <ecook@pivotal.io>

--- redis/redis_test.go
+++ redis/redis_test.go
@@ -1,7 +1,7 @@
 package redis_test
 
 import (
-	redigo "github.com/garyburd/redigo/redis"
+	redigo "github.com/gomodule/redigo/redis"
 	. "github.com/onsi/ginkgo"
 
 	"github.com/pivotal-cf/redisutils/redis"
@@ -14,13 +14,13 @@ var _ = Describe("interfaces", func() {
 		})
 
 		It("is implemented by redis.Fake", func() {
-			var _ redis.Redis = redis.NewFake()
+			var _ redis.Redis = new(redis.Fake)
 		})
 	})
 
 	Describe("Conn", func() {
 		It("is implemented by redis.ConnFake", func() {
-			var _ redigo.Conn = redis.NewConnFake()
+			var _ redigo.Conn = new(redis.ConnFake)
 		})
 	})
 })
